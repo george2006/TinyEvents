e06eeea671e96fcb336466c5d5bd6a878b7049ed
George <durbich@outlook.com>
2026-07-25 18:41:37 +0200

    Validate PostgreSQL worker connection configuration

--- src/TinyEvents.PostgreSql.AdoNet/Connections/TinyPostgreSqlAdoNetWorkerConnectionFactory.cs
+++ src/TinyEvents.PostgreSql.AdoNet/Connections/TinyPostgreSqlAdoNetWorkerConnectionFactory.cs
@@ -22,6 +22,8 @@ internal sealed class TinyPostgreSqlAdoNetWorkerConnectionFactory : ITinyPostgre
             throw new ArgumentNullException(nameof(serviceProvider));
         }
 
+        options.ValidateWorkerConfiguration();
+
         this.options = options;
         this.serviceProvider = serviceProvider;
     }
